693fa9668e9da7e00c7e13502333cb25f731d243
Linterview <45552000+Linterview@users.noreply.github.com>
2018-12-04 19:54:03 +0700

    add download LinterViewTrack

--- main.js
+++ main.js
@@ -1873,7 +1873,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\SVMC\Linterview_02\LinterView\client-angular\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! d:\SVMC\Linterview_02\LinterView\client-angular\src\main.ts */"./src/main.ts");
 
 
 /***/ })
